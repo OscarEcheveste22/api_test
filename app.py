67bc9c2b64ad19952705be2c8d79d1d206eafa71
Oscar Flores <l19251031@sjuanrio.tecnm.mx>
2025-02-25 17:50:17 -0600

    Se mustra Hola Mundo y Retorna alumnos

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app= Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hola Mundo"
+
+@app.route("/alumnos")
+
+def get_alumnos():
+    return "Retornando alumnos"
+
+if __name__=="__main__":
+    app.run()
